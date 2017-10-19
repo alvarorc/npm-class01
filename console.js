8f0f61db52c9915b24567e3633aeb73bdb874b16
alvarorc <alvarorc.sistemas@gmail.com>
2017-10-19 20:05:00 -0300

    Commit inicial

---
+++ console.js
@@ -0,0 +1,12 @@
+var $ = require('jquery');
+
+console.log("Hello world!!!");
+
+console.log("Here is jquery", $);
+
+function suma (a, b) {
+	console.log("La suma es ", a + b);
+}
+
+
+suma(10, 20);
